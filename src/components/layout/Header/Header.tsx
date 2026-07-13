3742f639af65ac91db19ba6f2bed5d4aac4f536c
Andrew Kerr <andrewkerr3956@gmail.com>
2026-07-13 18:03:38 -0400

    Add Header component styles and implement unit dropdown functionality

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import logoImg from '../../../assets/images/logo.svg';
+import styles from './Header.module.css';
 
-export default function Header() {
+const Header = () => {
+    const [unitDropdownActive, setUnitDropdownActive] = useState(false);
+
     return (
-        <header id="main-header">
+        <header id="main-header" className={styles.header}>
             <picture>
                 <img src={logoImg} alt="WeatherNow Logo" />
             </picture>
+            <button onClick={() => setUnitDropdownActive(!unitDropdownActive)}>
+                Units
+            </button>
+            {unitDropdownActive && (
+                <menu className={styles.unitDropdownMenu}>
+                    <div className={styles.unitDropdownGroup} aria-roledescription='Groups the temperature unit controls'>
+                        <strong className={styles.unitDropdownTitle}>Temperature</strong>
+                    </div>
+                    <hr />
+                </menu>
+            )}
         </header>
     )
-}
+};
+
+export default Header;
--- src/components/weather/DailyForecast/DailyForecast.tsx
+++ src/components/weather/DailyForecast/DailyForecast.tsx
@@ -1,24 +1,28 @@
 import type { ReactNode } from 'react';
 import styles from './DailyForecast.module.css';
 import { getDayOfWeek } from '../../../utils/date';
+import { getWeatherIconByCode } from '../../../utils/weather-icon';
 
 interface DailyForecastItemProps {
     day: string;
-    icon: ReactNode;
+    weather_code: number;
     maxTemp: number;
     minTemp: number;
 }
 
-const DailyForecastItem = ({ day, icon, maxTemp, minTemp }: DailyForecastItemProps) => {
+const DailyForecastItem = ({ day, weather_code, maxTemp, minTemp }: DailyForecastItemProps) => {
     return (
         <div className={styles.dailyForecastItem}>
-           <p>{day}</p> 
-           <p>
-              {maxTemp}
-           </p>
-           <p>
-              {minTemp}
-           </p>
+            <p>{day}</p>
+            <picture>
+                <img src={getWeatherIconByCode(weather_code)} height={60} width={60} />
+            </picture>
+            <p>
+                {maxTemp}
+            </p>
+            <p>
+                {minTemp}
+            </p>
         </div>
     )
 }
@@ -33,8 +37,9 @@ const DailyForecast = ({ data }: DailyForecastProps) => {
             <h3>Daily forecast</h3>
             <div className={styles.dailyForecastList}>
                 {data?.daily?.time?.length > 0 && data.daily.time?.map((_, idx) => (
-                    <DailyForecastItem 
+                    <DailyForecastItem
                         day={getDayOfWeek(new Date(data?.daily?.time?.[idx]).getDay())?.slice(0, 3)}
+                        weather_code={data?.daily?.weather_code?.[idx]}
                         maxTemp={data?.daily?.temperature_2m_max?.[0]}
                         minTemp={data?.daily?.temperature_2m_min?.[0]}
                     />
